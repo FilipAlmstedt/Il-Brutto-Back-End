0242d3838b592f1fbe11d2ef33f35020d4e014d6
fpasth <filip.almstedt@almxer.se>
2021-09-03 15:11:23 +0200

    implemented nodemailer in back-end controllers

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,6 +1,20 @@
 const Booking = require("../Model/booking");
+const nodemailer = require("nodemailer")
+const {
+    nodeMailerPassword,
+    nodeMailerUser
+} = require("../Config/config");
 
-// FInd all bookings in DB and send to front end
+// Transport to initiate nodemailer
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: nodeMailerUser,
+    pass: nodeMailerPassword,
+  },
+});
+
+// Find all bookings in DB and send to front end
 const getBookingTable = async (req, res) => {
   const bookings = await Booking.find();
   res.send(bookings);
@@ -19,6 +33,27 @@ const addBooking = async (req, res) => {
       customerInfo: customerInfo
     }).save();
   
+    // After we add a reservation to DB, collect that object and send a confirmation mail
+    const sendMailBookingInfo = await Booking.findOne({ bookingRef: bookingRef });
+
+    // Send confirmation mail with a link, to the confirmation page showing the booking information
+    await transport.sendMail({
+      from: nodeMailerUser, 
+      to: sendMailBookingInfo.customerInfo.email,
+      subject: "Thank you for your reservation!",
+      html: `
+          <h1>Thank you for your reservation ${sendMailBookingInfo.customerInfo.firstName}!</h1>
+          
+          <p>For more information regarding your reservation information, visit <a href="http://localhost:3000/confirmation/${sendMailBookingInfo.bookingRef}">this link</a></p>
+
+          <p> To visit our website, please click <a href="http://localhost:3000/">here!</a></p>
+
+          <p>Have a good meal!,</p>
+          <p>Il Brutto</p>
+
+        `,
+    });
+
 };
 
 // Collect one specfic booking object from DB that is used for the edit component in Front-End
@@ -47,6 +82,25 @@ const updateBooking = async (req, res) => {
 // Collect id which is booking ref and delete said reservation in DB
 const deleteBooking = async (req, res) => {
   const bookingRef = req.params.id;
+
+  // Collect bookingobject from DB and use to send email cancelled booking confirmation
+  const deletedBooking = await Booking.findOne({ bookingRef: bookingRef});
+
+  // Send confirmation mail with a link, to confirm that the reservation has been cancelled
+  await transport.sendMail({
+    from: nodeMailerUser, 
+    to: deletedBooking.customerInfo.email,
+    subject: "Your reservation has been cancelled!",
+    html: `
+        <h1>Sorry to hear you had to cancel the reservation ${deletedBooking.customerInfo.firstName}!</h1>
+
+        <p> To visit our website, please click <a href="http://localhost:3000/">here!</a></p>
+
+        <p>Have a good meal!,</p>
+        <p>Il Brutto</p>
+
+      `,
+  });
 
   //deleting booking based on bookingRef
   await Booking.deleteOne({ bookingRef: bookingRef });
